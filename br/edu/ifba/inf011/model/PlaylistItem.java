5c16543224c7e4ad462299f1310fa10004dd3b5d
Luís Fernando Cezar Dos Santos <47705336+nando-cezar@users.noreply.github.com>
2023-06-14 10:09:13 -0300

    Merge composite decorator (#10) (#12)
    * experimental: decorator implementation
    
    * Simplificar path no ResourceLoader
    
    * WIP: Implementando composite
    
    * refactor
    
    * decorator: refactor code
    
    * merge
    
    * decorator: refactor code
    
    * refactor code (#9)
    
    * refactor code
    
    * feat: add comments and increment class Aplicacao
    
    * refactor: modified concrete decorators in play method to invoke super.play()
    
    * refactor: modified resource loader for object creation
    
    * refactor: finish
    
    * feature: increment concrete component
    
    ---------
    
    Co-authored-by: luizfelipetorres <fellipe116@gmail.com>

---
+++ br/edu/ifba/inf011/model/PlaylistItem.java
@@ -0,0 +1,7 @@
+package br.edu.ifba.inf011.model;
+
+/* Component: Composite pattern */
+public interface PlaylistItem {
+	public String getNome();
+	public String execute();
+}
--- br/edu/ifba/inf011/model/resources/ResourceLoader.java
+++ br/edu/ifba/inf011/model/resources/ResourceLoader.java
@@ -8,40 +8,77 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.edu.ifba.inf011.model.MusicaNotas;
+import br.edu.ifba.inf011.model.*;
 
 public class ResourceLoader {
-	
-	public static String DIR_NAME = "D:\\workspace\\DPAvalII2023.1\\src\\br\\edu\\ifba\\inf011\\model\\resources\\data\\";
-		
-	
-	public static ResourceLoader loader;
-	
-	public static ResourceLoader instance() {
-		if(ResourceLoader.loader == null)
-			ResourceLoader.loader = new ResourceLoader();
-		return ResourceLoader.loader;
-	}
-	
-	public MusicaNotas createMusica(String nome) throws IOException {
-		MusicaNotas musica = new MusicaNotas(nome);
-		musica.setAcordes(this.loadNotas(nome));
-		return musica;
-	}
-	
-	public List<String> loadNotas(String nome) throws IOException {
-		return this.loadResource(nome, "notas");
-	}
-	
-	public List<String> loadLetra(String nome) throws IOException {
-		return this.loadResource(nome, "letra");
-	}
 
-	public List<String> loadResource(String nome, String extensao) throws IOException {
-		List<String> resource = new ArrayList<String>();
+    public static String DIR_NAME = System.getProperty("user.dir") + "\\br\\edu\\ifba\\inf011\\model\\resources\\data\\";
+
+    public static ResourceLoader loader;
+
+    public static ResourceLoader instance() {
+        if (ResourceLoader.loader == null) {
+            ResourceLoader.loader = new ResourceLoader();
+        }
+        return ResourceLoader.loader;
+
+    }
+
+    public Musica createMusicaSomenteComNota(String nome) throws IOException {
+        Musica musica = new MusicaTemplate(nome);
+
+        Musica musicaSomenteComNota = new MusicaNotas(musica);
+        musicaSomenteComNota.setConteudo(this.loadNotas(nome));
+
+        return musicaSomenteComNota;
+    }
+
+    public Musica createMusicaComNotaLetra(String nome) throws IOException {
+        Musica musica = new MusicaTemplate(nome);
+
+        Musica musicaComNotaLetra = new MusicaNotas(musica);
+        musicaComNotaLetra.setConteudo(this.loadNotas(nome));
+
+        Musica musicaLetraOriginal = new MusicaLetraOriginal(musicaComNotaLetra);
+        musicaLetraOriginal.setConteudo(this.loadLetra(nome));
+
+        return musicaLetraOriginal;
+    }
+
+    public Musica createMusicaComNotaLetraOriginalTraduzida(String nome, String extensao) throws IOException {
+        Musica musica = new MusicaTemplate(nome);
+
+        Musica musicaComNotaLetraOriginalTraduzida = new MusicaNotas(musica);
+        musicaComNotaLetraOriginalTraduzida.setConteudo(this.loadNotas(nome));
+
+        Musica musicaLetraOriginal = new MusicaLetraOriginal(musicaComNotaLetraOriginalTraduzida);
+        musicaLetraOriginal.setConteudo(this.loadLetra(nome));
+
+        Musica musicaLetraTraduzida = new MusicaLetraTraduzida(musicaLetraOriginal);
+        musicaLetraTraduzida.setConteudo(this.loadTraducao(nome, extensao));
+
+        return musicaLetraTraduzida;
+    }
+
+    public List<String> loadNotas(String nome) throws IOException {
+        return this.loadResource(nome, "notas");
+    }
+
+    public List<String> loadLetra(String nome) throws IOException {
+        return this.loadResource(nome, "letra");
+    }
+
+    public List<String> loadTraducao(String nome, String extensao) throws IOException {
+        return this.loadResource(nome, extensao);
+    }
+
+    public List<String> loadResource(String nome, String extensao) throws IOException {
+        List<String> resource = new ArrayList<String>();
         Path path = Paths.get(ResourceLoader.DIR_NAME + nome + "." + extensao);
-        Files.lines(path, StandardCharsets.ISO_8859_1).forEach(resource::add);
+        Files
+                .lines(path, StandardCharsets.ISO_8859_1)
+                .forEach(resource::add);
         return resource;
-	}	
+    }
 
 }
